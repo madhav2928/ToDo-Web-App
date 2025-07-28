23c18cf9b208becaf69f39bfefe7c5ae23238fe5
Madhav Anchala <madhav2928@gmail.com>
2025-07-28 12:38:25 +0530

    Add dynamic apibaseUrl

--- frontend/src/components/TodoApp.jsx
+++ frontend/src/components/TodoApp.jsx
@@ -2,8 +2,7 @@ import React, { useState, useEffect, use } from 'react';
 import axios from 'axios';
 import { Plus, Check, Trash2, Edit3 } from 'lucide-react';
 
-
-const API_BASE_URL = 'http://localhost:8080/api/todos';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const TodoApp = () => {
     const [todos, setTodos] = useState([]);
